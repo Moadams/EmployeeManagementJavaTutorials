c08ba53f7074e45317f7c8a670aac5b82db39242
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-04-23 16:06:32 +0000

    recommit exception

--- controller/PayrollService.java
+++ controller/PayrollService.java
@@ -1,11 +1,16 @@
 package controller;
 
+import exceptions.InvalidSalaryRateException;
 import models.Employee;
 
 public class PayrollService {
-    public double calculateNetSalary(Employee employee) {
-        double tax = employee.getTaxRate() * employee.getSalary();
-        double netSalary = employee.getSalary() + employee.getBonus() - tax;
-        return netSalary;
+    public double calculateNetSalary(Employee employee) throws InvalidSalaryRateException {
+        if(employee.getTaxRate() > 1){
+            throw new InvalidSalaryRateException("Tax rate should not be greater than 1");
+        }else{
+            double tax = employee.getTaxRate() * employee.getSalary();
+            double netSalary = employee.getSalary() + employee.getBonus() - tax;
+            return netSalary;
+        }
     }
 }
